ba5be5f5a2f45ff2f0f67a2d8faf44b2e9ce5d3e
i19605 <i19605@nu.edu.pk>
2022-05-29 22:14:26 +0500

    adding user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SignIn from './components/signIn'
+import Adduser from './components/adduser'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Error404 from './Error/error'
 
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/adduser" element={<Adduser />} />
         <Route path="/*" element={<Error404/>} />
       </Routes>
     </BrowserRouter>
