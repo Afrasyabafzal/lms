09d8fa91d68ff32d5af8f21d9d1fd8255f4e1fe1
i19605 <i19605@nu.edu.pk>
2022-05-29 21:56:22 +0500

    implementing admin course

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CourseAdmin from './components/admincourses'
 import SignIn from './components/signIn'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Error404 from './Error/error'
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/adminC" element={<CourseAdmin />} />
         <Route path="/*" element={<Error404/>} />
       </Routes>
     </BrowserRouter>
