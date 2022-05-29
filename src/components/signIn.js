0d4aa6c15dafbcfcec51fc54a68e3b16cf6c6864
i19605 <i19605@nu.edu.pk>
2022-05-29 19:46:56 +0500

    implement signup form

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -18,7 +18,7 @@ function SignIn() {
               <a href="/signUp" className="font-medium text-indigo-600 hover:text-indigo-500">
                 sign up for a new account
               </a>
-            </p>
+         </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
