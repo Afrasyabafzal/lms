871c8eb3feee3482724ad638b80ceb14d5f6b228
Hassan Ahmed <hassanahmad61931@gmail.com>
2022-05-31 00:08:34 +0500

    Updated Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,43 +1,17 @@
-/*
-  This Footer requires Tailwind CSS v2.0+ 
-  
-  This Footer requires some changes to your config:
-  
-  
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  
-*/
 const navigation = {
-    solutions: [
-      { name: 'Marketing', href: '#' },
-      { name: 'Analytics', href: '#' },
-      { name: 'Commerce', href: '#' },
-      { name: 'Insights', href: '#' },
-    ],
-    support: [
-      { name: 'Pricing', href: '#' },
-      { name: 'Documentation', href: '#' },
-      { name: 'Guides', href: '#' },
-      { name: 'API Status', href: '#' },
+    Resources: [
+    { name: 'Courses', href: '#' },
+    { name: 'Material', href: '#' },
+    { name: 'Assessments', href: '#' },
     ],
     company: [
-      { name: 'About', href: '#' },
-      { name: 'Blog', href: '#' },
-      { name: 'Jobs', href: '#' },
-      { name: 'Press', href: '#' },
+      { name: 'About Us', href: '#' },
       { name: 'Partners', href: '#' },
+      { name: 'Contact', href: '#' },
     ],
     legal: [
-      { name: 'Claim', href: '#' },
-      { name: 'Privacy', href: '#' },
       { name: 'Terms', href: '#' },
+      { name: 'Privacy', href: '#' },
     ],
     social: [
       {
@@ -88,25 +62,12 @@ const navigation = {
           </svg>
         ),
       },
-      {
-        name: 'Dribbble',
-        href: '#',
-        icon: (props) => (
-          <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-            <path
-              fillRule="evenodd"
-              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-              clipRule="evenodd"
-            />
-          </svg>
-        ),
-      },
     ],
   }
   
   export default function Footer() {
     return (
-      <footer className="bg-gray-800" aria-labelledby="footer-heading">
+      <footer className="bg-indigo-900" aria-labelledby="footer-heading">
         <h2 id="footer-heading" className="sr-only">
           Footer
         </h2>
@@ -114,10 +75,10 @@ const navigation = {
           <div className="xl:grid xl:grid-cols-3 xl:gap-8">
             <div className="grid grid-cols-2 gap-8 xl:col-span-2">
               <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Solutions</h3>
+              <div>
+                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Resources</h3>
                   <ul role="list" className="mt-4 space-y-4">
-                    {navigation.solutions.map((item) => (
+                    {navigation.Resources.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-300 hover:text-white">
                           {item.name}
@@ -127,20 +88,6 @@ const navigation = {
                   </ul>
                 </div>
                 <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
-                  <ul role="list" className="mt-4 space-y-4">
-                    {navigation.support.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-300 hover:text-white">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
                   <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.company.map((item) => (
@@ -152,7 +99,9 @@ const navigation = {
                     ))}
                   </ul>
                 </div>
-                <div className="mt-12 md:mt-0">
+              </div>
+              <div className="md:grid md:grid-cols-2 md:gap-8">
+                <div>
                   <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.legal.map((item) => (
@@ -164,6 +113,7 @@ const navigation = {
                     ))}
                   </ul>
                 </div>
+                
               </div>
             </div>
             <div className="mt-8 xl:mt-0">
@@ -171,7 +121,7 @@ const navigation = {
                 Subscribe to our newsletter
               </h3>
               <p className="mt-4 text-base text-gray-300">
-                The latest news, articles, and resources, sent to your inbox weekly.
+                The latest news, offered courses, sent to your inbox weekly.
               </p>
               <form className="mt-4 sm:flex sm:max-w-md">
                 <label htmlFor="email-address" className="sr-only">
@@ -207,7 +157,7 @@ const navigation = {
               ))}
             </div>
             <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
-              &copy; 2020 Workflow, Inc. All rights reserved.
+              &copy; 2022 Brilliant Pro, Inc. All rights reserved.
             </p>
           </div>
         </div>
