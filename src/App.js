a0899a41599826942e2e45fc5bf3b50fa49558a8
i19605 <i19605@nu.edu.pk>
2022-05-31 11:53:02 +0500

    aboutus

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import CourseAdmin from './components/admincourses'
 import SignIn from './components/signIn'
 import Adduser from './components/adduser'
 import SignUp from './components/signUp'
+import Aboutus from './components/aboutus'
 import AdminSignIn from './components/AdminSignin'
 
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
@@ -13,8 +14,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+
         <Route path="/adminC" element={<CourseAdmin />} />
         <Route path="/adduser" element={<Adduser />} />
+        <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/admin" element={<AdminSignIn />} />
         <Route path="/*" element={<Error404/>} />
